5b8b1030c3c5594acfc1942009f7a0b168d8ebb0
Nguyen Tien Dat <nguyentiendatpite24@gmail.com>
2025-03-06 15:17:05 +0700

    Thiết kế database sơ bộ
    khởi tạo dtb và input

---
+++ Database/SE1940_Database.sql
@@ -0,0 +1,115 @@
+﻿USE [master]
+GO
+
+/*******************************************************************************
+   Drop database if it exists
+********************************************************************************/
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'SE1940_Prj')
+BEGIN
+	ALTER DATABASE [SE1940_Prj] SET OFFLINE WITH ROLLBACK IMMEDIATE;
+	ALTER DATABASE [SE1940_Prj] SET ONLINE;
+	DROP DATABASE [SE1940_Prj] ;
+END
+
+GO
+
+CREATE DATABASE [SE1940_Prj]
+GO
+
+USE [SE1940_Prj]
+GO
+
+/*******************************************************************************  
+    Drop all tables if they exist  
+*******************************************************************************/
+
+-- Khai báo biến để chứa truy vấn động  
+DECLARE @sql nvarchar(MAX)  
+SET @sql = N''  
+
+-- Tạo truy vấn để xóa tất cả các ràng buộc khóa ngoại (Foreign Key Constraints)  
+SELECT @sql = @sql + N'ALTER TABLE ' + QUOTENAME(KCU1.TABLE_SCHEMA)  -- Lấy schema của bảng  
+    + N'.' + QUOTENAME(KCU1.TABLE_NAME)  -- Lấy tên bảng  
+    + N' DROP CONSTRAINT '  -- Lệnh xóa ràng buộc  
+    + QUOTENAME(rc.CONSTRAINT_NAME) + N'; ' + CHAR(13) + CHAR(10)  -- Lấy tên của ràng buộc  
+
+FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS AS RC  -- Lấy danh sách khóa ngoại  
+
+-- Liên kết với bảng chứa thông tin khóa chính và bảng liên quan  
+INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KCU1  
+    ON KCU1.CONSTRAINT_CATALOG = RC.CONSTRAINT_CATALOG  
+    AND KCU1.CONSTRAINT_SCHEMA = RC.CONSTRAINT_SCHEMA  
+    AND KCU1.CONSTRAINT_NAME = RC.CONSTRAINT_NAME  
+
+-- Thực thi lệnh để xóa tất cả ràng buộc khóa ngoại  
+EXECUTE(@sql)  
+
+GO  -- Chia khối lệnh trong SQL Server
+
+-- Khai báo biến để chứa truy vấn xóa tất cả các bảng  
+DECLARE @sql2 NVARCHAR(max)=''  
+
+-- Tạo truy vấn để xóa toàn bộ bảng trong database  
+SELECT @sql2 += 'DROP TABLE ' + QUOTENAME(TABLE_SCHEMA) + '.'+ QUOTENAME(TABLE_NAME) + '; '  
+FROM INFORMATION_SCHEMA.TABLES  
+WHERE TABLE_TYPE = 'BASE TABLE'  -- Chỉ lấy những bảng thông thường (không phải view)  
+
+-- Thực thi truy vấn xóa toàn bộ bảng  
+EXEC Sp_executesql @sql2  
+
+GO  -- Kết thúc khối lệnh
+
+--Role
+CREATE TABLE [Role] (
+    RoleID integer PRIMARY KEY IDENTITY(1, 1),
+    RoleName VARCHAR(255) not null,
+	Password VARCHAR(255)
+);
+GO
+
+--Department
+CREATE TABLE [Department] (
+    DepartmentID integer PRIMARY KEY IDENTITY(1, 1),
+    DepartmentName VARCHAR(1000) not null
+);
+GO
+
+--Status
+CREATE TABLE [Status] (
+    StatusID integer PRIMARY KEY IDENTITY(1, 1),
+    StatusName VARCHAR(255) not null
+);
+GO
+
+--User
+CREATE TABLE [Users] (
+    UserID integer PRIMARY KEY IDENTITY(1, 1),
+    Username VARCHAR(255) UNIQUE,
+	Password VARCHAR(255),
+    Name NVARCHAR(255) not null,
+	Email VARCHAR(255) UNIQUE,
+	Phone VARCHAR(20) UNIQUE,
+	DepartmentID INT not null,
+	RoleID int,
+	Status INT
+	FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID),
+	FOREIGN KEY (RoleID) REFERENCES Role(RoleID)
+);
+GO
+
+--Application
+CREATE TABLE [Application] (
+    ApplicationID integer PRIMARY KEY IDENTITY(1, 1),
+    UserID INT not null,
+	StartDate DATE,
+	EndDate DATE,
+	Title NVARCHAR(300),
+	Reason NVARCHAR(1500),
+	StatusID INT,
+	ApproverID INT,
+	IsCancel INT
+	FOREIGN KEY (UserID) REFERENCES Users(UserID),
+	FOREIGN KEY (ApproverID) REFERENCES Users(UserID)
+);
+GO
+
---
+++ Database/SE1940_Input.sql
@@ -0,0 +1,46 @@
+﻿--Role
+SET IDENTITY_INSERT [dbo].[Role] ON 
+INSERT INTO [dbo].[Role] (RoleID, RoleName) VALUES (0,N'Admin')
+INSERT INTO [dbo].[Role] (RoleID, RoleName) VALUES (1,N'Department Manager')
+INSERT INTO [dbo].[Role] (RoleID, RoleName) VALUES (2,N'Group Leader')
+INSERT INTO [dbo].[Role] (RoleID, RoleName) VALUES (3,N'Staff')
+SET IDENTITY_INSERT [dbo].[Role] OFF
+GO
+
+--Department
+INSERT INTO [dbo].[Department] (DepartmentName) VALUES (N'IT')
+INSERT INTO [dbo].[Department] (DepartmentName) VALUES (N'QA')
+INSERT INTO [dbo].[Department] (DepartmentName) VALUES (N'Sale')
+
+--Status
+INSERT INTO [dbo].[Status] (StatusName) VALUES (N'Inprogress')
+INSERT INTO [dbo].[Status] (StatusName) VALUES (N'Approved')
+INSERT INTO [dbo].[Status] (StatusName) VALUES (N'Rejected')
+
+--User
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'admin', N'123', N'Admin', N'admin@gmail.com', '0987-654-321', 1, 0, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'dpit', N'123', N'Trịnh Trần Phương Tuấn', N'j97@gmail.com', '0987-654-322', 1, 1, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'dpqa', N'123', N'Sơn Tùng MTP', N'mtp@gmail.com', '0987-654-323', 2, 1, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'dpsale', N'123', N'Soobin Hoàng Sơn', N'soobin@gmail.com', '0987-654-444', 3, 1, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'grit', N'123', N'Hà Anh Tuấn', N'hat@gmail.com', '0987-654-324', 1, 2, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'grqa', N'123', N'Phan Mạnh Quỳnh', N'pmq@gmail.com', '0987-654-325', 2, 2, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'grsale', N'123', N'KICM', N'kicm@gmail.com', '0987-654-326', 3, 2, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'stit1', N'123', N'MCK', N'mck@gmail.com', '0987-654-327', 1, 3, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'stit2', N'123', N'Wxdie', N'wxdie@gmail.com', '0987-654-328', 1, 3, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'stqa1', N'123', N'TLinh', N'tlinh@gmail.com', '0987-654-329', 2, 3, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'stqa2', N'123', N'Bình Gold', N'binhgold@gmail.com', '0987-654-330', 2, 3, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'stsale1', N'123', N'Phúc Du', N'phucdu@gmail.com', '0987-654-331', 3, 3, 1)
+INSERT INTO [dbo].Users (Username, Password, Name, Email, Phone, DepartmentID, RoleID, Status) 
+VALUES (N'stsale2', N'123', N'Bray', N'bray@gmail.com', '0987-654-332', 3, 3, 1)
